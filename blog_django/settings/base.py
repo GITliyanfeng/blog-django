eb955c83b80ccd98fa4160c3d8f759a95c7f9f58
liyanfeng <957012567@qq.com>
2019-03-14 19:35:11 +0800

    添加theme目录,目录中为前端添加bootstrap样式

--- blog_django/settings/base.py
+++ blog_django/settings/base.py
@@ -21,7 +21,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.join(BASE_DIR, '../')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -58,11 +57,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blog_django.urls'
-
+THEME = 'bootstrap'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -116,4 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = '/tmp/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'themes', THEME, 'static')
+]
